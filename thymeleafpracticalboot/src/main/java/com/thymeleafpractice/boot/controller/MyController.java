25c89fff52ac9e4f4982a165683b1bd562ba19c9
Myworldisjarvis <namesadaf@gmail.com>
2024-09-13 18:30:08 +0530

    dynamic value for specific fragment using { th:fragment="footer(a,b,more)" } in thymeleaf

--- thymeleafpracticalboot/src/main/java/com/thymeleafpractice/boot/controller/MyController.java
+++ thymeleafpracticalboot/src/main/java/com/thymeleafpractice/boot/controller/MyController.java
@@ -1,5 +1,6 @@
 package com.thymeleafpractice.boot.controller;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
@@ -12,49 +13,47 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class MyController {
 
-	
 //	handler for basic statements
-@RequestMapping(value = "/about", method = RequestMethod.GET)
-public String about(Model model) {
-	
-	model.addAttribute("name" , "Sadaf Hussain");
-	model.addAttribute("cDate",new Date().toLocaleString());
-	System.out.println("going to about hend...");
-	return "about";
-}
-	
+	@RequestMapping(value = "/about", method = RequestMethod.GET)
+	public String about(Model model) {
+
+		model.addAttribute("name", "Sadaf Hussain");
+		model.addAttribute("cDate", new Date().toLocaleString());
+		System.out.println("going to about hend...");
+		return "about";
+	}
 
 //handler for ( each loop ) statements
-@GetMapping("/example-loop")
-public String itrerateHandler(Model m) {
-	
-	List<String> list = List.of("Hi","by","chai","game");
-//	List<String> list = null;
-	m.addAttribute("list",list);
-	
-	return "itreate";
-}
+	@GetMapping("/example-loop")
+	public String itrerateHandler(Model m) {
 
+		List<String> list = List.of("Hi", "by", "chai", "game");
+//	List<String> list = null;
+		m.addAttribute("list", list);
+
+		return "itreate";
+	}
 
 // handler for conditional statements 
-@GetMapping("/condition")
-public String conditionHandler(Model m) {
-	m.addAttribute("isActive", false);
-	m.addAttribute("gender","M");
-	
-	List<Integer> list = List.of(1,2,3,4,5,6,7);
-	m.addAttribute("numList",list);
-	
-	return "condition";
-}
+	@GetMapping("/condition")
+	public String conditionHandler(Model m) {
+		m.addAttribute("isActive", false);
+		m.addAttribute("gender", "M");
+
+		List<Integer> list = List.of(1, 2, 3, 4, 5, 6, 7);
+		m.addAttribute("numList", list);
+
+		return "condition";
+	}
 
 // handler for including fragments
-@GetMapping("/service")
-public String servicesHandler(Model m) {
-	
-	
-	return "services";
-}
+	@GetMapping("/service")
+	public String servicesHandler(Model m) {
 
+		m.addAttribute("title", "I Am Programmer");
+		m.addAttribute("subtitle", LocalDate.now().toString());
+
+		return "services";
+	}
 
 }
